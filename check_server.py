ad4891d971579c5a3b5afea4fb6e96db7de0d0e2
Sekikawa <work.e.sekikawa@gmail.com>
2023-02-13 02:12:05 +0900

    設問2の実装、設問1の修正

--- check_server.py
+++ check_server.py
@@ -12,12 +12,39 @@ def find_timed_out_ip(filename):
             response_time = row[2]
             prefix_length = int(ip.split('/')[1])
             address = ip.split('/')[0]
-            
+             #タイムアウトしたとき
             if response_time == '-':
-                recorded_ips[address] = (time, prefix_length)
-            #ipアドレス本体とプレフィックス長を確認
-            elif address in recorded_ips and recorded_ips[address][1] == prefix_length:
+                if not address in recorded_ips:
+                    recorded_ips[address] = [time, prefix_length]
+            #ipアドレス本体を確認
+            elif address in recorded_ips :
                 print(f"{ip},{int((time-recorded_ips[address][0]).total_seconds())}")
                 del recorded_ips[address]
 
-find_timed_out_ip("./log.csv")
+#設問2
+def find_timed_out_ip_N(filename, N: int):
+    recorded_ips = {}
+    with open(filename) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            time = datetime.strptime(row[0], '%Y%m%d%H%M%S')
+            ip = row[1]
+            response_time = row[2]
+            prefix_length = int(ip.split('/')[1])
+            address = ip.split('/')[0]
+
+            #タイムアウトしたとき
+            if response_time == '-':
+                if address in recorded_ips:
+                    recorded_ips[address][2] += 1
+                else:
+                    recorded_ips[address] = [time, prefix_length, 1]
+            #ipアドレス本体を確認
+            elif address in recorded_ips:
+                if recorded_ips[address][2] >= N:
+                    print(f"{ip},{int((time-recorded_ips[address][0]).total_seconds())}")
+                del recorded_ips[address]
+                
+if __name__ == '__main__':
+    #find_timed_out_ip("./log.csv")
+    find_timed_out_ip_N("./log.csv", 1)
